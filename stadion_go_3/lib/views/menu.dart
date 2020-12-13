160444238ee74775a249b17dd88d08b35be82082
HrazderaAlexander <alexander.hrazdera@gmail.com>
2020-12-13 21:53:58 +0100

    add nfc read and write

--- stadion_go_3/lib/views/menu.dart
+++ stadion_go_3/lib/views/menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_nfc_reader/flutter_nfc_reader.dart';
 
 class MenuScreen extends StatefulWidget{
   MenuScreen();
@@ -10,13 +11,70 @@ class MenuScreen extends StatefulWidget{
 
 class MenuScreenState extends State<MenuScreen> {
   MenuScreenState();
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: null,
-      body: Text(
-          "Home"
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(child: NfcScan()),
       ),
     );
   }
+}
 
+class NfcScan extends StatefulWidget {
+  NfcScan({Key key}) : super(key: key);
+
+  @override
+  _NfcScanState createState() => _NfcScanState();
+}
+
+class _NfcScanState extends State<NfcScan> {
+  TextEditingController writerController = TextEditingController();
+
+  @override
+  initState() {
+    super.initState();
+    writerController.text = 'Flutter NFC Scan';
+    FlutterNfcReader.onTagDiscovered().listen((onData) {
+      print(onData.id);
+      print(onData.content);
+    });
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    writerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: writerController,
+        ),
+        RaisedButton(
+          onPressed: () {
+            FlutterNfcReader.read();
+          },
+          child: Text("Read"),
+        ),
+        RaisedButton(
+          onPressed: () {
+            FlutterNfcReader.write(" ", writerController.text).then((value) {
+              print(value.content);
+            });
+          },
+          child: Text("Write"),
+        )
+      ],
+    );
+  }
 }
